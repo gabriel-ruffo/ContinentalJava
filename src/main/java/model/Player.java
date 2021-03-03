b1064814f5422be2902f01bb6e32e374f8517b1d
gabriel-ruffo <gabriel.ruffo@outlook.com>
2021-03-03 10:56:02 -0500

    Add point information for Player.java

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -5,4 +5,33 @@ public class Player {
     private int points;
     private Hand hand;
 
+    public Player () {
+        points = 0;
+        hand = new Hand();
+    }
+
+    public Player(int points, Hand hand) {
+        this.points = points;
+        this.hand = hand;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints(int pointsToAdd) {
+        if(pointsAreValid(pointsToAdd)) {
+            points += pointsToAdd;
+        }
+        // TODO: how to enforce correct points? Custom exception, boolean return type, ...
+    }
+
+    private boolean pointsAreValid(int points) {
+        return points % 5 == 0;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
 }
